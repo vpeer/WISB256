67c8d21a55ba6cff08b05a5f2851d6de1d3c3b7e
vpeer <v.d.peereboom@students.uu.nl>
2015-05-07 12:13:56 +0000

    Versnelde versie

--- Opdracht2/Sieve_of_E.py
+++ Opdracht2/Sieve_of_E.py
@@ -1,13 +1,13 @@
 import time
 import sys
-file = open("prime.dat" , "w")
+file = open(sys.argv[2], "w")
 
 N = int(sys.argv[1])
 aantal = 0
 Lijst = list(range(0, N+1))
 start = time.time()
 for i in range(2, N+1):
-    if i in Lijst:
+    if Lijst[i] !=0:
         file.write(str(i)+"\n")
         aantal = aantal + 1
         for j in range(2, int(N/i)+1):
